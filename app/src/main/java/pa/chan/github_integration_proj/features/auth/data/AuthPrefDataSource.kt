4b0e7e0f19a5777cd2b2b1a4ef0321301179737a
pogch <pogchamb1@gmail.com>
2023-03-31 11:15:41 +0300

    change string resources

--- app/src/main/java/pa/chan/github_integration_proj/features/auth/data/AuthPrefDataSource.kt
+++ app/src/main/java/pa/chan/github_integration_proj/features/auth/data/AuthPrefDataSource.kt
@@ -1,27 +1,28 @@
 package pa.chan.github_integration_proj.features.auth.data
 
 import android.content.SharedPreferences
+import pa.chan.githubintagrationproj.R
 import javax.inject.Inject
 
 class AuthPrefDataSource @Inject constructor(private val sharedPreferences: SharedPreferences) {
 
     fun setUserToken(token: String) {
         sharedPreferences.edit()
-            .putString("UserToken", token)
+            .putString(R.string.userTokenPref.toString(), token)
             .apply()
     }
 
     fun setUserName(username: String){
         sharedPreferences.edit()
-            .putString("UserName", username)
+            .putString(R.string.userNamePref.toString(), username)
             .apply()
     }
 
     fun getUserToken(): String?  {
-        return sharedPreferences.getString("UserToken", "")
+        return sharedPreferences.getString(R.string.userTokenPref.toString(), "")
     }
 
     fun getUserName(): String? {
-        return sharedPreferences.getString("UserName", "")
+        return sharedPreferences.getString(R.string.userNamePref.toString(), "")
     }
 }
